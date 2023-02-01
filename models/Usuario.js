3d148f615bca92dee52629baa51ed0fcd8856178
Antonio Alfonso González Rodríguez <alum.agonzalezr.2021@iesalixar.org>
2023-02-01 18:02:06 +0100

    probando push con modelo usuario

---
+++ models/Usuario.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var Usuario = require("../models/Usuario.js");
+var PostSchema = new Schema({
+  user: {
+    type: Schema.ObjectId,
+    ref: "Usuario",
+  },
+  dni: { type: String, required: true },
+  nombre: String,
+  apellidos: String,
+  direcion: String,
+  correo: String,
+  telefono: String,
+});
+module.exports = mongoose.model("Usuario", PostSchema);
