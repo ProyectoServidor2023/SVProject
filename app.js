e66cc508cc35cd2bdfd447c6b7e00e6b9e6a28c1
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2023-02-28 20:18:04 +0100

    finalizando validaciones ventas

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var VentaRouter = require("./routes/ventas");
 var app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const { body, validationResult } = require("express-validator");
+
 
 var mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -31,6 +33,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/ventas", (req, res) => {
+  res.render("ventas");
+})
+
+app.post('/registrar_venta', [
+  body('vehiculo', 'Ingreese Id de vehiculo válido').exists().isLength({ min: 10, max: 40}),
+  body('propietario', 'Ingreese Id de propietario válido').exists().isLength({ min: 10, max: 40}),
+  body('comprador', 'Ingreese Id de comprador válido').exists().isLength({ min: 10, max: 40}),
+  body('fecha', 'Ingreese Fecha').exists().isLength({ min: 8, max: 8 }),
+  body('precio', 'Ingreese Precio').exists().isLength({ min: 1, max: 10 }),
+], (req, res) => {
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(req.body)
+      const valores = req.body  
+      const validaciones = errors.array()  
+      res.render('ventas', {validaciones:validaciones, valores:valores})  
+    } else {
+      res.send("Validaciones correctas")
+    }
+  
+})
 
 app.use("/", indexRouter);
 app.use("/usuarios", usersRouter);
